870f002cd98cdb8db1be2294a55ef756aa8b338b
Yosua Blanco <yosuabd@estudiantec.cr>
2022-06-16 01:54:09 -0600

    File modified and MVC

---
+++ FileSystem_Application/src/Main/FileSystem_Application.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import View.FileSystem_Window;
+
+
+/**
+ *
+ * @author Natalia
+ */
+public class FileSystem_Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    static FileSystem_Window window;
+    public static void main(String[] args) {
+        // TODO code application logic here
+        window = new FileSystem_Window();
+        window.setVisible (true);
+    }
+    
+}
---
+++ FileSystem_Application/src/Model/Folder.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import Model.File;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Yosua Blanco Diaz
+ */
+public class Folder {
+    protected String name; // Folder identifier
+    protected boolean root; // Folder root 
+    protected Folder father; // Folder father 
+    protected String path; // folder direccion 
+    protected ArrayList<Folder> folders; // Folders in this folder
+    protected ArrayList<File> files;  // Files in this folder
+    
+    public Folder(String name, boolean root, Folder father, String path, ArrayList<Folder> folders, ArrayList<File> files){
+        this.name = name;
+        this.root = root;
+        this.father = father;
+        this.path = path;
+        this.folders = folders;
+        this.files = files;
+    }
+
+    public Folder(String name, boolean root, Folder father, String path) {
+        this.name = name;
+        this.root = root;
+        this.father = father;
+        this.path = path;
+        this.folders = null;
+        this.files = null;
+    }
+
+    public Folder(String name, boolean root, String path) {
+        this.name = name;
+        this.root = root;
+        this.path = path;
+        this.father = null;
+        this.folders = null;
+        this.files = null;
+    }
+    
+    
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public boolean getRoot(){
+        return root;
+    }
+    
+    public void setRoot(boolean root){
+        this.root = root;
+    }
+    
+    public Folder getFather(){
+        return father;
+    }
+    
+    public void setFather(Folder father){
+        this.father = father;
+    }
+    
+    public String getPath(){
+        return path;
+    }
+    
+    public void setPath(String path){
+        this.path = path;
+    }
+    
+    public ArrayList<Folder> getFolders(){
+        return folders;
+    }
+    
+    public void setFolders(ArrayList<Folder> folders){
+        this.folders = folders;
+    }
+    
+    public void addFolders(Folder folders){
+        this.folders.add(folders);
+    }
+    
+    public ArrayList<File> getFiles(){
+        return files;
+    }
+    
+    public void setFiles(ArrayList<File>  files){
+        this.files = files;
+    }
+    
+    public void addFiles(File  files){
+        this.files.add(files);
+    }
+    
+    @Override
+    public String toString(){
+        return "Folder {" + "name:" + name + ", root:" + root +", father:" +father.getName() + 
+                ", Path:"+ path +", Folders:" +folders+ ", Files:" + files;
+    }
+}
